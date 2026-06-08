794d34d31275db18bb3fa9423184009f3d65a6b4
Liz Casler Loomans <162842106+Liziloo@users.noreply.github.com>
2026-06-08 18:25:17 +0000

    Fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var gulp = require('gulp');
-var fs = require('fs');
 
 gulp.task('css', function () {
     var sass = require('gulp-sass')(require('sass'));
@@ -19,46 +18,12 @@ gulp.task('css', function () {
         .pipe(gulp.dest('./asset/css'));
 });
 
-gulp.task('bump-version', function (cb) {
-    var iniPath = './config/theme.ini';
-    if (!fs.existsSync(iniPath)) {
-        return cb(new Error('theme.ini not found'));
-    }
-
-    var iniContent = fs.readFileSync(iniPath, 'utf8');
-    var isMajor = process.argv.includes('--major');
-
-    iniContent = iniContent.replace(/(version\s*=\s*["'])([^"']+)(["'])/, function (match, prefix, version, suffix) {
-        var parts = version.split('.');
-
-        if (parts.length < 2) {
-            parts = ['1', '0'];
-        }
-
-        if (isMajor) {
-            parts[0] = parseInt(parts[0], 10) + 1;
-            parts[1] = 0;
-            if (parts[2] !== undefined) parts[2] = 0;
-        } else {
-            parts[1] = parseInt(parts[1], 10) + 1;
-            if (parts[2] !== undefined) parts[2] = 0;
-        }
-
-        var newVersion = parts.join('.');
-        console.log('Bumping theme.ini version from ' + version + ' to ' + newVersion + (isMajor ? ' (Major)' : ' (Minor)'));
-        return prefix + newVersion + suffix;
-    });
-
-    fs.writeFileSync(iniPath, iniContent, 'utf8');
-    cb();
-});
-
 gulp.task('css:watch', function () {
     gulp.watch('./asset/sass/*.scss', gulp.parallel('css'));
 });
 
-// For local dev (recompiles on save without touching theme.ini version)
+// Standard local development task
 gulp.task('default', gulp.series('css', 'css:watch'));
 
-// For deployment (recompiles and increments theme.ini version once before git upload)
-gulp.task('build', gulp.series('css', 'bump-version'));
+// Standard build task (just compiles the CSS without version bumping)
+gulp.task('build', gulp.series('css'));
